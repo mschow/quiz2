39e534a6aa49653bf6aa249fcfd8260c3d63a004
Micah Schow <micah.schow@gmail.com>
2015-09-29 02:30:37 -0600

    Finished Quiz
    All problems solved.

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -2,6 +2,18 @@
 
 function arraySum(arr) {
 
+var totalSum = 0;
+
+for(var i=0; i < arr.length; i++){
+  if(Array.isArray(arr[i])){
+    totalSum += arraySum(arr[i])
+  } else {
+    totalSum += arr[i];
+  }
+}
+
+return totalSum;
+
 }
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -2,6 +2,21 @@
 
 function longestString(arr) {
 
+var stringLength = 0;
+var long;
+
+for (var i=0; i < arr.length; i++){
+
+    if(typeof arr[i] === 'string'){
+      if(arr[i].length > stringLength){
+        stringLength = arr[i].length;
+        long = arr[i];
+      }
+    }
+}
+
+return long;
+
 }
 
 module.exports = longestString;
